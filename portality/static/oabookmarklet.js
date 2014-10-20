870ea27b41e1069031fa9341837c22a6681474e1
Mark MacGillivray <mark@cottagelabs.com>
2014-10-20 14:54:12 +0100

    bookmarklet

--- portality/static/oabookmarklet.js
+++ portality/static/oabookmarklet.js
@@ -2,6 +2,87 @@ jQuery(document).ready(function() {
 
 $.getScript("https://openaccessbutton.org/static/oabutton.js", function() {
     
+    var detectAuthorEmail = function() {
+        var nodes, node, matches, i;
+    
+        // match email address in mailto link
+        // from http://stackoverflow.com/a/201447/145899
+        var mailto_re = /^mailto:(\S+@\S+\.\S+)/;
+    
+        // look for meta[name=citation_author_email][content]
+        // test on http://dx.doi.org/10.1007/978-3-642-02879-3_7
+        nodes = document.getElementsByTagName("meta");
+        for (i = 0; i < nodes.length; i++) {
+          node = nodes[i];
+    
+          if (node.getAttribute("name") == "citation_author_email") {
+            return node.getAttribute("content");
+          }
+        }
+    
+        // look for links that start with "mailto:".
+        // can't guarantee this is the author - might be an editor or support email.
+        // test on http://dx.doi.org/10.1371/journal.pone.0052814
+        nodes = document.getElementsByTagName("a");
+        for (i = 0; i < nodes.length; i++) {
+          node = nodes[i];
+    
+          if (matches = mailto_re.exec(node.getAttribute("href"))) {
+            return matches[1].replace(/\?.*/, ""); // remove any query string
+          }
+        }
+    
+        return '';
+    };
+
+    
+    var detectDOI = function() {
+        var nodes, node, childNode, matches, i, j;
+    
+        // match DOI: test on http://t.co/eIJciunBRJ
+        var doi_re = /\b10\.\d{4,}(?:\.\d+)*\/\S+\b/;
+    
+        // look for meta[name=citation_doi][content]
+        nodes = document.getElementsByTagName("meta");
+        for (i = 0; i < nodes.length; i++) {
+          node = nodes[i];
+    
+          if (node.getAttribute("name") == "citation_doi") {
+            return node.getAttribute("content").replace(/^doi:/, "");
+          }
+        }
+    
+        // look in all text nodes for a DOI
+        nodes = document.getElementsByTagName("*");
+        for (i = 0; i < nodes.length; i++) {
+          node = nodes[i];
+    
+          if (!node.hasChildNodes()) {
+            continue;
+          }
+    
+          if (node.nodeName == "SCRIPT") {
+            continue;
+          }
+    
+          for (j = 0; j < node.childNodes.length; j++) {
+            childNode = node.childNodes[j];
+    
+            // only text nodes
+            if (childNode.nodeType !== 3) {
+              continue;
+            }
+    
+            if (matches = doi_re.exec(childNode.nodeValue)) {
+              return matches[0];
+            }
+          }
+        }
+    
+        return '';
+    };
+
+    
     var guid = (function() {
       function s4() {
         return Math.floor((1 + Math.random()) * 0x10000)
@@ -14,13 +95,6 @@ $.getScript("https://openaccessbutton.org/static/oabutton.js", function() {
       };
     })();
 
-    // TODO: this bookmarklet should be updated with the code from the old project that 
-    // pulls title author etc from the page the bookmarklet is opened on
-    // then those useful bits of info should be shown in text input fields 
-    // in a hidden panel of the bookmarklet panel below
-    // if no info can be scraped, some may be returned from the oab.status call
-    // but if none at all can be found, the hidden panel should open and the user 
-    // should be asked to fill in the fields
 
     $('body').append('<div id="oabookmarkletcontainer">' +
         '<h2 id="oabookmarkletheader">Open Access Button</h2>' +
@@ -65,6 +139,8 @@ $.getScript("https://openaccessbutton.org/static/oabutton.js", function() {
             data: {
                 url: window.location.href,
                 story: $('#oabookmarkletstory').val(),
+                doi: detectDOI(),
+                authoremails: detectAuthorEmail(),
                 id: rid
             }
         });
